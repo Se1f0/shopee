bb75b3f95c853587e52ce2d4b0de8218d89f26e5
SAFE <sesegueni@gmail.com>
2023-02-23 14:12:33 +0100

    Fix search keyword not remaining in the search bar

--- resources/views/livewire/header-search-component.blade.php
+++ resources/views/livewire/header-search-component.blade.php
@@ -1,4 +1,4 @@
 <form action="{{ route('product.search') }}" class="hm-searchbox">
-    <input type="text" placeholder="Enter your search key ..." name="q">
+    <input type="text" placeholder="Enter your search key ..." name="q" value="{{ $q }}">
     <button class="li-btn" type="submit"><i class="fa fa-search"></i></button>
 </form>
