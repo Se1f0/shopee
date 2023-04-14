fbd752a7686c48e187f47fa203691a2eae34e2cb
SAFE <sesegueni@gmail.com>
2023-04-14 18:07:21 +0100

    Admin can manage contacts

--- app/Http/Livewire/Admin/AdminContactComponent.php
+++ app/Http/Livewire/Admin/AdminContactComponent.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Contact;
 use Livewire\Component;
 
 class AdminContactComponent extends Component
 {
     public function render()
     {
-        return view('livewire.admin.admin-contact-component');
+        $contacts = Contact::orderBy('created_at', 'desc')->paginate(10);
+        return view('livewire.admin.admin-contact-component', ['contacts' => $contacts]);
     }
 }
--- resources/views/livewire/admin/admin-contact-component.blade.php
+++ resources/views/livewire/admin/admin-contact-component.blade.php
@@ -1,3 +1,78 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div style="margin-top: -20px">
+    <!-- Begin Li's Breadcrumb Area -->
+    <div class="breadcrumb-area">
+        <div class="container">
+            <div class="breadcrumb-content">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li class="active">Contacts</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- Li's Breadcrumb Area End Here -->
+    <!--Shopping Cart Area Strat-->
+    <div class="Shopping-cart-area pt-60 pb-60">
+        {{-- <button type="button" class="btn btn-success" style="margin-bottom: 5px; margin-left: 1375px"
+            onclick="location.href='{{ route('admin.category.add') }}'">Add New
+            Category</button> --}}
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <form action="#">
+                        <div class="table-content table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+
+                                        <th class="li-product-thumbnail">#</th>
+                                        <th class="li-product-thumbnail">ID</th>
+                                        <th class="cart-product-name">Subject</th>
+                                        <th class="cart-product-name">Name</th>
+                                        <th class="li-product-price">Email</th>
+                                        <th class="li-product-price">Phone</th>
+                                        <th class="li-product-price">Message</th>
+                                        <th class="li-product-price">Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $i = ($contacts->currentPage() - 1) * $contacts->perPage();
+                                    @endphp
+                                    @foreach ($contacts as $contact)
+                                        <tr>
+                                            <td class="li-product-price"><span class="amount">{{ ++$i }}</span>
+                                            </td>
+                                            <td class="li-product-price"><span class="amount">{{ $contact->id }}</span>
+                                            </td>
+                                            <td class="li-product-price"><span
+                                                    class="amount">{{ $contact->subject }}</span>
+                                            </td>
+                                            <td class="li-product-price"><span
+                                                    class="amount">{{ $contact->name }}</span>
+                                            </td>
+                                            <td class="li-product-price"><span
+                                                    class="amount">{{ $contact->email }}</span>
+                                            </td>
+                                            <td class="li-product-price"><span
+                                                    class="amount">{{ $contact->phone }}</span>
+                                            </td>
+                                            <td class="li-product-price"><span
+                                                    class="amount">{{ $contact->comment }}</span>
+                                            </td>
+                                            <td class="li-product-price"><span
+                                                    class="amount">{{ $contact->created_at }}</span>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            {{ $contacts->links('custom-pagination') }}
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Shopping Cart Area End-->
 </div>
