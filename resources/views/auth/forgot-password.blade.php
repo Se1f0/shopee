c294b3b9e11ec489685619714490996418cb8537
SAFE <sesegueni@gmail.com>
2023-04-16 12:46:19 +0100

    Password reset func implemented

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
@@ -22,4 +22,46 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+<x-app-layout>
+    <div class="breadcrumb-area" style="margin-top: -20px">
+        <div class="container">
+            <div class="breadcrumb-content">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li class="active">Forget Password</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="page-section mb-60">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-12 col-md-12 col-xs-12 col-lg-6 mb-30">
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
+                        <div class="login-form">
+                            <h4 class="login-title">Forget Password</h4>
+                            @if (session('status'))
+                                <div class="mb-4 font-medium text-sm text-success">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                            <div class="row">
+                                <div class="col-md-12 col-12 mb-20">
+                                    <label>Email Address*</label>
+                                    <input class="mb-0" type="email" placeholder="Email Address" name="email"
+                                        required autofocus>
+                                </div>
+                                <div class="col-md-12">
+                                    <button class="register-button mt-0">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('password.store') }}">
         @csrf
 
@@ -36,4 +36,56 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+<x-app-layout>
+    <div class="breadcrumb-area" style="margin-top: -20px">
+        <div class="container">
+            <div class="breadcrumb-content">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li class="active">Reset your password</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="page-section mb-60" style="margin-top: 20px">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-12 col-md-12 col-lg-6 col-xs-12">
+                    <form method="POST" action="{{ route('password.store') }}">
+                        @csrf
+                        <!-- Password Reset Token -->
+                        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                        <div class="login-form">
+                            <h4 class="login-title">Reset your password</h4>
+                            <div class="row">
+                                <div class="col-md-12 mb-20">
+                                    <label>Email Address*</label>
+                                    <input class="mb-0" type="email" name="email" placeholder="Email Address"
+                                        value="{{ $request->email }}" required autofocus>
+                                </div>
+                                <div class="col-md-6 mb-20">
+                                    <label>Password</label>
+                                    <input class="mb-0" type="password" name="password" placeholder="Password"
+                                        required autocomplete="new-password">
+                                </div>
+                                <div class="col-md-6 mb-20">
+                                    <label>Confirm Password</label>
+                                    <input class="mb-0" type="password" name="password_confirmation"
+                                        placeholder="Confirm Password" required>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="check-box d-inline-block ml-0 ml-md-2 mt-10">
+                                        <button class="register-button mt-0">Reset password</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
